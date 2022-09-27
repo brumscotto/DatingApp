4f3e5b9a676043396b1fb61c79e30e94f7c1daf0
brumscotto <scott.kirker@gmail.com>
2022-09-27 10:56:02 -0400

    section 4 -pre token

---
+++ API/Data/Migrations/20220923140702_UserPasswordFieldsFixed.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordFieldsFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+        }
+    }
+}
